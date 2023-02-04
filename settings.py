a5bbcb133145c4f264299a7f5193066d9cb00e92
Egor <105914606+Egor-Tarasov@users.noreply.github.com>
2023-02-04 14:27:08 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,12 +3,12 @@ import pymysql
 from peewee import *
 
 
-bot = telebot.TeleBot('5822209451:AAH2m83luZY2HHC8WrO52SOcpSJp6sCi_Os')
+bot = telebot.TeleBot('YOUR_API_KEY')
 
 headers: dict = {
     'content-type': 'application/json',
     'x-rapidapi-host': 'hotels4.p.rapidapi.com',
-    'x-rapidapi-key': 'c02baf658emsh19fca9c5f2e0924p1ccd3djsn07c2983e5bf9'
+    'x-rapidapi-key': 'YUOR_RAPIDAPI_KEY'
 }
 
 
